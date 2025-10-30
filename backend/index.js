988759f4ef094a6e8fe8b6d13458af66234db39b
Chinmay-211 <chinmaynaik211@gmail.com>
2025-10-30 21:47:29 +0530

    Updated for deploment

--- backend/index.js
+++ backend/index.js
@@ -103,11 +103,4 @@ app.get('/api/stock/:companyName', async (req, res) => {
 });
 
 
-// --- Serve React Frontend ---
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 module.exports = app;
